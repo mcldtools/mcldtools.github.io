f2ce58fae08d79b4d6ad93be5f5284534644da89
John Coonrod <johncoonrod@penguin>
2021-02-14 12:23:15 -0500

    basics

--- form.js
+++ form.js
@@ -2,6 +2,6 @@ function saveform(formid){
     expiry="Fri, 01 Jan 2038 00:00:01 GMT";
     const form = document.getElementById(formid);
     Array.from(form.elements).forEach((input) => {
-      document.cookie=input.name+'='+URIEncode(input.value)+';expires='+expiry+';path=/';
+      document.cookie=input.name+'='+encodeURI(input.value)+';expires='+expiry+';path=/';
     });
 }
