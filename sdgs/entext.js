f9bb78e9878e8dc7712c727f7a6b5de823c09bb6
Movement for Community-led Development <78973745+mcldtools@users.noreply.github.com>
2021-03-24 09:21:35 -0400

    Create entext.js

---
+++ sdgs/entext.js
@@ -0,0 +1,64 @@
+const basics={
+    h1:"Basic Information",
+    program:"Community Name",
+    country:"Programming Country(s)",
+    region:"District/Region (if applicable)",
+    name:"Name(s) completing this assessment",
+    date:"Date Completed",
+    comment:"Additional comments/feedback",
+    save:"Save data"
+}
+const questions=[
+"Are safety nets reaching who they should?
+Poverty estimate based on observed quality of housing
+Number of meetings and participants of production groups or related local council committees, eg: Agriculture, Fisheries and Livestock
+Zero Hunger [Alt: FAO Household Food Insecurity Experience scale]
+Diet diversity
+Knowledge of 1000 Day Nutrition
+Good Health and Wellbeing
+Observed staffed hours of nearest clinic
+Observed pre-natal visit rates
+Number of meetings and participants of Education & Health Standing Committee
+Quality Education
+Observed secondary school enrollment, M/F
+Gender Equality
+Zero tolerance of GBV
+Zero tolerance of Child Marriage
+Confidence of women that their priorities are being addressed by leadership
+Number of meetings and participants in Family Conflict Resolution, Women and Children Standing Committee
+Clean Water and Sanitation
+% tested wells for arsenic
+% improved sanitation usage 
+Affordable and Clean Energy
+% household with electricity
+% household usage of clean cookstoves
+Decent Work and Economic Growth
+Access, Affordability and Awareness of vocational training opportunities
+Access to affordable credit and formal banking
+Identified gaps in decent work
+Industry, Innovation and Infrastructure
+Quality of Roads
+Internet access, M/F
+Reduced Inequalities
+Growth of savings of poorest 20%
+Sustainable Communities
+Existence of village risk assessment and reduction plan
+Responsible Consumption and Production
+Elimination of plastic bags
+Climate Action
+Existence of village climate change assessment, adaptation strategy and degree of implementation.
+Number of meetings and participants in Disaster Management and Climate Change Standing Committee
+Life Below Water
+Sustainability of fish supply
+Life on Land
+Observed percentage of forest cover
+Peace, Justice and Strong Institutions
+Existence of trained peace facilitators
+Satisfaction with local dispute resolution and recommendations
+Size of UP budget and its potential and ability to tax locally
+Observed number of functionaries accessible to village on weekly basis
+Partnerships for the Global Goals
+Establishment of Village Development Committee (VDC)
+Satisfaction with functioning of VDC
+Posted Priorities of VDC
+Satisfaction with Progress on posted priorities
