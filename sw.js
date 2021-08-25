a0184ef881604cfc905fd2d7190411f254cf1770
John Coonrod <john.coonrod@thp.org>
2021-08-24 21:46:04 -0400

    fix sw.js

--- sw.js
+++ sw.js
@@ -21,10 +21,8 @@ const URLS = [
 ];
 
 self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
   e.waitUntil((async () => {
     const cache = await caches.open(cacheName);
-    console.log('[Service Worker] Caching all URLs');
     await cache.addAll(URLS);
   })());
 });
