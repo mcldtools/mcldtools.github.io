3b7e69f27a02dff4b7e3fac63bdf0198126388a8
John Coonrod <54367582+jcoonrod@users.noreply.github.com>
2025-07-22 10:23:59 -0400

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-const version = 'v49';
+const version = 'v50';
 const langlist = ['am','ar','bd','ee','en','es','fr','lg','ny','sw'];
 const maxpage = 38; // 37 is More 38 is Full Report
 lang = localStorage.getItem("lang");
